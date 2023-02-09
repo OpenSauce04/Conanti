c4cf24e07d4666ca465e2cd34784e8645247d19e
OpenSauce <opensauce04@gmail.com>
2023-02-09 09:48:50 +0000

    Match case is now switch case; '_' case is now default case

--- Conanti/BuildFunctions/ReplaceTokens.cs
+++ Conanti/BuildFunctions/ReplaceTokens.cs
@@ -17,6 +17,15 @@ namespace Conanti
 						case "{": tokenizedContent[lineIndex][tokenIndex] = ":"; break;
 						case "}": tokenizedContent[lineIndex].RemoveAt(tokenIndex); break;
 						case "function": tokenizedContent[lineIndex][tokenIndex] = "def"; break;
+						case "switch": tokenizedContent[lineIndex][tokenIndex] = "match"; break;
+					}
+					if (
+						tokenIndex != 0 &&
+						tokenizedContent[lineIndex][tokenIndex-1] == "case" &&
+						tokenizedContent[lineIndex][tokenIndex] == "default"
+					)
+					{
+						tokenizedContent[lineIndex][tokenIndex] = "_";
 					}
 				}
 			}
